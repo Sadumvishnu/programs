112c520434477c2f8deede985f29241398b4b03e
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-27 01:23:49 -1200

    fifth commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+
+public class Test{
+    public static void main(String[] args){
+        char ch='a';
+        int ascii=ch;
+        System.out.println("the ascii value of a"+ch + ascii);
+    }
+}
