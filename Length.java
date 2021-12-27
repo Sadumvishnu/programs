b476c353524f92d5cb57a2c55e0c8dff1a1813db
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-27 01:13:00 -1200

    first commit

---
+++ Length.java
@@ -0,0 +1,9 @@
+public class Length {
+    public static void main(String[] args){
+String s1 = "vishnu";
+String s2 = "anusha";
+System.out.println("length of strings are " + s1.length());
+System.out.println("length of strings are " + s2.length());
+    }
+    
+}
