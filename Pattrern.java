7b4141a3a7da562fcb4176a7fda40b989e71fb43
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-27 01:26:38 -1200

    sixth commit

---
+++ Pattrern.java
@@ -0,0 +1,12 @@
+class Pattrern{
+    public static void main(String[] args){
+        int n = 9;
+        for(int i=0;i<n;i++){
+        for(int j=i;j>0;j++){
+        System.out.print("*");
+
+        }
+        System.out.println();
+    }
+}
+}
