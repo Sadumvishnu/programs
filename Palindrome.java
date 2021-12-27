f470eb6eaea926a8b9a293a8c3e16c2967f2c987
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-27 01:21:29 -1200

    fourth commit

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+
+public class Palindrome {
+    public static void main(String[] args){
+        System.out.println("input of time:");
+        int hour = 03;
+        int minute = 23;
+        String str = "sunday";
+        int year = 2021;
+        int date = 17;
+        System.out.println(hour);
+        System.out.println(minute);
+        System.out.println(str);
+        System.out.println(year);
+        System.out.println(date);
+            
+
+    }
+}
