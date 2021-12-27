3629d7e3d8556be47dcb4803fe4e0589d180da1f
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-27 01:17:33 -1200

    second commit

---
+++ Length2.java
@@ -0,0 +1,10 @@
+public class Length2 {
+    public static void main(String[] args){
+        String str = "sadum vishnu";
+        char ch = str.charAt(1);
+        int length = str.length();
+        System.out.println(ch );
+        System.out.println("length of the string is " + length);
+    }
+    
+}
