43b1b6263ffbd00c3ebd1223d37eb40267e419f9
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-28 23:51:53 -1200

    commit

---
+++ Formate.java
@@ -0,0 +1,12 @@
+public class Formate {
+    public static void main(String[] args){
+        String str = "ima in learning stage";
+        int vaue = 988;
+        System.out.println(String.format("the given string is %s" , "iam in learning stage"));
+        System.out.println(String.format("the vale is %d", vaue));
+        
+
+
+    }
+    
+}
