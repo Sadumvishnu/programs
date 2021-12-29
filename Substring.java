4112c8bcb506addd9f607a4f9700a961259382b3
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-29 00:31:45 -1200

    commit

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+
+    public static void main(String[] args){
+        String str1 = "vidyaganesh";
+        System.out.println(str1.substring(1));
+        System.out.println(str1.substring(2,7));
+    }
+}
