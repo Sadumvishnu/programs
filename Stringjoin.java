fac018c6de0acd5abccf9c44c613596bd6925238
sadumvishnu <vishnudaddy891@gmail.com>
2021-12-30 01:07:10 -1200

    committed

---
+++ Stringjoin.java
@@ -0,0 +1,8 @@
+public class Stringjoin {
+public static void main(String[] args){
+     String str = String.join(",","welcome" , "to" , "the" , "world");
+     String date = String.join("/","30","12","2021");
+     System.out.println(str);
+     System.out.println(date);
+}    
+}
