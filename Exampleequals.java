836ce737612546e4b894df92b67ed8f9f2a82974
sadumvishnu <vishnudaddy891@gmail.com>
2022-01-05 00:28:27 -1200

    first commit

---
+++ Exampleequals.java
@@ -0,0 +1,10 @@
+public class Exampleequals {
+    public static void main(String[]args){
+        String s1 = "sujatha";
+        String s2 = "Sujathasubramanyam";
+        String s3 = "Sujathasubramanyam";
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+    }
+    
+}
