ca082322bb8901704a3ec3dea493dac11bd6b54f
Sadum Vishnu <vishnudaddy891@gmail.com>
2022-01-05 00:32:04 -1200

    second commit

---
+++ Exampleindexof.java
@@ -0,0 +1,13 @@
+public class Exampleindexof {
+    public static void main(String[] args){
+        String str = "i love my mother";
+    int index1 = str.indexOf("love");
+    int ex2 = str.indexOf("mother");
+    System.out.println(index1 +","+ ex2);
+
+
+    }
+    
+
+    
+}
